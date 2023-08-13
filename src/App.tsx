03800a93ee602817a30c48256bac6f4cfb6a019f
Syaril Nazirul Sazali <syarilnazirul.sazali@Syarils-MBP.realtek>
2023-08-13 22:27:04 +0800

    fix overflow WIP

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import { AccordionCard } from "./components/AccordionCard";
 import Breadcrumb from "./components/Breadcrumb";
 import { Header } from "./components/Header";
 import LeftNav from "./components/LeftNav";
-import {
-  HiHome,
-  HiCheck,
-  HiOutlineUser,
-  HiOutlineInbox,
-  HiOutlineClipboard,
-} from "react-icons/hi";
+import { HiHome } from "react-icons/hi";
 import { StepItem, StepsList } from "./components/StepList";
 
 function App() {
@@ -48,21 +42,50 @@ function App() {
   return (
     <div className="h-screen flex flex-col bg-gray-100">
       <Header />
-      <main className="flex flex-row flex-grow">
+      <main className="flex flex-row flex-grow overflow-y-hidden">
         <section className="bg-white p-4">
           <LeftNav items={leftNavItems} />
         </section>
         <div className="flex-grow p-6">
           <Breadcrumb items={breadcrumbItems} />
+          <h1 className="my-6 text-2xl font-bold">Permit To Work</h1>
           <div className="flex flex-grow">
-            <section className="flex-grow p-5">
-              <h1 className="my-6 text-2xl font-bold">Permit To Work</h1>
+            <section className="flex-grow pr-5 overflow-y-auto max-h-screen">
+              <AccordionCard title="Summary" subtitle="PTW 230911001">
+                <h1>hsdkjfkds</h1>
+              </AccordionCard>
+              <AccordionCard title="Summary" subtitle="PTW 230911001">
+                <h1>hsdkjfkds</h1>
+              </AccordionCard>
+              <AccordionCard title="Summary" subtitle="PTW 230911001">
+                <h1>hsdkjfkds</h1>
+              </AccordionCard>
+              <AccordionCard title="Summary" subtitle="PTW 230911001">
+                <h1>hsdkjfkds</h1>
+              </AccordionCard>
+              <AccordionCard title="Summary" subtitle="PTW 230911001">
+                <h1>hsdkjfkds</h1>
+              </AccordionCard>
+              <AccordionCard title="Summary" subtitle="PTW 230911001">
+                <h1>hsdkjfkds</h1>
+              </AccordionCard>
+              <AccordionCard title="Summary" subtitle="PTW 230911001">
+                <h1>hsdkjfkds</h1>
+              </AccordionCard>
               <AccordionCard title="Summary" subtitle="PTW 230911001">
                 <h1>hsdkjfkds</h1>
               </AccordionCard>
             </section>
-            <section className="p-4 w-64 whitespace-normal">
-              <StepsList steps={steps} />
+            <section className="sm:hidden md:flex flex-col w-64 whitespace-normal">
+              <button className="flex justify-center items-center mb-5 bg-white border-2 border-gray-300 text-black px-4 py-2 rounded-md">
+                Change Log
+              </button>
+              <div className="">
+                <h3 className="pb-8 text-gray-500 text-2xl">WORK DETAILS</h3>
+                <div className="px-5">
+                  <StepsList steps={steps} />
+                </div>
+              </div>
             </section>
           </div>
         </div>
